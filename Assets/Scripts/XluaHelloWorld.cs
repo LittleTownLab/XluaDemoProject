7eed2fd71cdaae024bc30212627df22e12ec7722
LittleTownLab <ydcstudio2016@gmail.com>
2019-03-18 12:56:52 +0800

    config xlua and print hello world by lua

---
+++ Assets/Scripts/XluaHelloWorld.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+namespace XluaPro
+{
+	public class XluaHelloWorld : MonoBehaviour 
+	{
+        LuaEnv env = null;
+        string strLua1 = "print('this is lua run')";
+        string strLua2 = "CS.UnityEngine.Debug.Log('this is c# run')";
+        private void Start()
+        {
+            env = new LuaEnv();
+
+            env.DoString(strLua1);
+            env.DoString(strLua2);
+
+        }
+
+        private void OnDestroy()
+        {
+            env.Dispose();
+        }
+    }
+}
